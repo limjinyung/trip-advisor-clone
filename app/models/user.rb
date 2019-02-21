d1b7333a348fb1665a57337c3cb299b47ff9616e
limjy0114 <jinyung0069@hotmail.com>
2019-02-21 16:55:35 +0800

    added enum role users and admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :listings
 
+  enum role: [:user, :admin]
+
   validates :email, presence: true, uniqueness: true
 end
--- db/migrate/20190221072010_create_users.rb
+++ db/migrate/20190221072010_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :username
       t.string :first_name
       t.string :last_name
-      t.string :about_mer
+      t.string :about_me
       t.string :email
       t.string :password_digest
 
